2b8b68faf2a7279470afb69fc756368d9cc2f313
crazy007 <1683221087@qq.com>
2018-08-08 17:26:46 +0800

    加入initData()方法

--- app/src/main/java/com/example/testexercise/MainActivity.java
+++ app/src/main/java/com/example/testexercise/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.testexercise;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 开发测试之用
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
     }
+
+    private void initData(){
+
+    }
+
 }
